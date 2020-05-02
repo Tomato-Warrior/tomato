01f7ff5977787809dc2825fe366a28c797034b3c
yuchan <lumir1031@gmail.com>
2020-05-02 18:23:44 +0800

    add project model

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,6 @@
+class ProjectsController < ActionController::Base
+  def index
+    
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
+  # Project
+  resources :projects
 end

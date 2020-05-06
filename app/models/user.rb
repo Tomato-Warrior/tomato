cd161856fe2d81c36ed2151c0d4db17e65e266d7
yuchan <lumir1031@gmail.com>
2020-05-06 17:26:47 +0800

    fix relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   # ralationship
-  has_many :counter
+  has_many :counters
+  has_many :projects
+  # callback
+  after_create :default_project_create
+  
+
+  def default_project_create
+    @project = Project.new(project_name: 'Default Box')
+    @project.save
+  end
+
 end

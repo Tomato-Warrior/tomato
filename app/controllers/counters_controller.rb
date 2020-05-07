5036231d38744e8543b61ffeff49421cd6ed77eb
yuchan <lumir1031@gmail.com>
2020-05-07 10:52:11 +0800

    fix rebase

--- app/controllers/counters_controller.rb
+++ app/controllers/counters_controller.rb
@@ -1,15 +1,12 @@
 class CountersController < ApplicationController
   def index
   end
-<<<<<<< HEAD
+
   def new
     @counter = Counter.new
   end
 
-=======
-  
   def create
-  
   end
->>>>>>> fix task/project view
+
 end

969d587a9c03d6e9af1d2e404af109987e5aa791
yuchan <lumir1031@gmail.com>
2020-05-07 10:52:11 +0800

    fix rebase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
 
-  # ralationship
-=======
   # relationship
->>>>>>> fix task/project view
   has_many :counters
   has_many :projects
   has_many :task
@@ -15,10 +11,8 @@ class User < ApplicationRecord
   # callback
   after_create :default_project_create
 
-<<<<<<< HEAD
-=======
   private
->>>>>>> add default project
+
   def default_project_create
     project = self.projects.build(project_name: 'Default Box')
     project.save

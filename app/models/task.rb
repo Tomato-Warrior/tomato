56743e5cba4d3ec22e958326a1e5987475298390
yuchan <lumir1031@gmail.com>
2020-05-07 18:05:57 +0800

    fix db

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,12 @@
 class Task < ApplicationRecord
+    #relationship
     has_many :tag_to_tasks
     has_many :tags, through: :tag_to_tasks
     has_many :counters
-    
+
     belongs_to :project
     belongs_to :user
+    #validates
     validates :task_name, presence: true
 
 

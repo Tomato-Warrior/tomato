1f75adbf87a0f2f5b0dcca90d27ec1ddbd651b14
Linda Wu <pinky510100@gmail.com>
2020-05-08 17:50:39 +0800

    fix conflict

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("projects")
 require("tictacs")
+require("counters")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference

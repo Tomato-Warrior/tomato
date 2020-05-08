2131302000e74b456c832ab83166a559b3c21062
kenaser12345 <53910941+kenaser12345@users.noreply.github.com>
2020-05-08 10:37:12 +0800

    fix project_color

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -41,7 +41,7 @@ class ProjectsController < ApplicationController
   private
 
   def project_params
-    params.require(:project).permit(:project_name)
+    params.require(:project).permit(:project_name, :project_cover)
   end
 
   def find_project

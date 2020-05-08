075a064bbebdbe3e82ff298c2eb375ee495ca67f
Linda Wu <pinky510100@gmail.com>
2020-05-08 13:51:12 +0800

    fix clock

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("projects")
+require("counters")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,5 +22,3 @@ import "bootstrap";
 import "./script/main.scss";
 import "controllers"
 import '@fortawesome/fontawesome-free/js/all'
-
-import "./counters.scss"

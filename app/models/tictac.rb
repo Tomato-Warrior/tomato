9a5053afd52b104056536fa0cc5600bcbb67479b
yuchan <lumir1031@gmail.com>
2020-05-08 19:18:58 +0800

    tictac add aasm

--- app/models/tictac.rb
+++ app/models/tictac.rb
@@ -1,6 +1,20 @@
 class Tictac < ApplicationRecord
+  include AASM
   #relationship
   belongs_to :user
-  belongs_to :task, required: false
+  belongs_to :task , required: false
 
+  #aasm
+  aasm column: 'status' do
+    state :pending, initial: true
+    state :failed, :finished
+  
+    event :processing do
+      transitions from: :pending, to: :finished
+    end
+  
+    event :processing do
+      transitions from: :pending, to: :failed
+    end
+  end
 end

a1d53551df68f5d13943573bba5a51d5b8ca6245
kenaser12345 <kenaser12345@gmail.com>
2020-05-08 18:53:22 +0800

    add stop_tomato

--- app/javascript/controllers/tictacs_controller.js
+++ app/javascript/controllers/tictacs_controller.js
@@ -6,103 +6,74 @@ export default class extends Controller {
   initialize(){
     this.clicked = false;
   }
-  count(e) {
+ 
+  start(e) {
     e.preventDefault();
-
-    let countdown;
+    
     const timeDisplay = document.querySelector('.display_time_left');
-    const btn25 = document.querySelector('.btn25');
-    const seconds = parseInt(btn25.dataset.time);
-    counter(seconds);
-    
-    function counter(seconds, status="work") {
-      clearInterval(countdown);
-      const now = Date.now();
-      const then = now + seconds * 1000;
-      
-      countdown = setInterval(() => {
-        const secondsLeft = Math.round((then - Date.now()) / 1000);
+    const startbtn = document.querySelector('.startbtn');
+    const stopbtn = document.querySelector(".stopbtn")
+    const seconds = parseInt(startbtn.dataset.time);
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    let isRunning = true
 
-        if (secondsLeft <= 0 && status === "work") {
-          clearInterval(countdown);
-          relaxTime()
-        } else if(secondsLeft < 0 && status === "relax") {
-          clearInterval(countdown)
-          return;
-        }
-        displayTimeLeft(secondsLeft);
-      }, 1000);
+    /*設定計時器*/ 
+    let counter = setInterval(() =>{
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
+      
+      if (secondsLeft < 0) {
+        clearInterval(countdown);
+      }
+      displayTimeLeft(secondsLeft); 
+    },1000)
+
+    /*按鈕顯示*/ 
+    if (isRunning == true){
+      startbtn.classList.add("d-none")
+      stopbtn.classList.remove("d-none")
     }
 
+    /*中止計時*/ 
+    stopbtn.addEventListener('click', stop)
+
+
+    function stop(){
+      clearInterval(counter)
+      displayTimeLeft(0)
+      console.log("stop")
+      isRunning = false
+      console.log(isRunning)
+      stopbtn.removeEventListener('click',stop)
+    }
+
+    /*顯示時間*/
     function displayTimeLeft(seconds) {
       const minutes = Math.floor(seconds / 60);
       const remainSeconds = seconds % 60;
       const display = `${minutes}:${remainSeconds < 10 ? 0 : ''}${remainSeconds}`
       timeDisplay.textContent = display;
     }
-    function relaxTime() {
-      counter(300,"relax")
-    }
   }
 
-  count5(e) {
+
+  stop(e){
     e.preventDefault();
     
-    let countdown;
-    const timeDisplay = document.querySelector('.display_time_left2');
-    const btn5 = document.querySelector('.btn5');
-    const seconds = parseInt(btn5.dataset.time);
-    counter(seconds);
+    const timeDisplay = document.querySelector('.display_time_left');
+    const startbtn = document.querySelector('.startbtn');
+    const stopbtn = document.querySelector(".stopbtn")
+    const seconds = parseInt(startbtn.dataset.time);
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    let isRunning = false
 
-    function counter(seconds, status="work") {
-      clearInterval(countdown);
-      const now = Date.now();
-      const then = now + seconds * 1000;
-      
-      countdown = setInterval(() => {
-        const secondsLeft = Math.round((then - Date.now()) / 1000);
 
-        if (secondsLeft <= 0 && status === "work") {
-          clearInterval(countdown);
-          relaxTime()
-        } else if(secondsLeft < 0 && status === "relax") {
-          clearInterval(countdown)
-          return;
-        }
-        displayTimeLeft(secondsLeft);
-      }, 1000);
-    }
 
-    function displayTimeLeft(seconds) {
-      const minutes = Math.floor(seconds / 60);
-      const remainSeconds = seconds % 60;
-      const display = `${minutes}:${remainSeconds < 10 ? 0 : ''}${remainSeconds}`
-      timeDisplay.textContent = display;
-    }
-    function relaxTime() {
-      counter(300,"relax")
-    }
-  }
-  change25Btn(e) {
-    e.preventDefault
-    const icon25 = document.querySelector('.icon25');
-    if (icon25.classList.contains('fa-play-circle')) {
-      icon25.classList.remove('fa-play-circle')
-      icon25.classList.add('fa-stop-circle')
-    } else {
-      icon25.classList.remove('fa-stop-circle')
-      icon25.classList.add('fa-play-circle') 
-    }
-  }
-  change5Btn(e) {
-    e.preventDefault
-    const icon5 = document.querySelector('.icon5');
-    if (icon5.classList.contains('fa-play-circle')) {
-      icon5.classList.remove('fa-play-circle')
-      icon5.classList.add('fa-stop-circle')
-    } else {
-      icon5.classList.remove('fa-stop-circle')
-      icon5.classList.add('fa-play-circle') 
+    /*按鈕顯示*/ 
+    if (isRunning == false){
+      stopbtn.classList.add("d-none")
+      startbtn.classList.remove("d-none")
     }
   }
 }

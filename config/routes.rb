59e2e3eb0424146f23225343f75bb61583559ab0
yuchan <lumir1031@gmail.com>
2020-05-11 13:50:58 +0800

    add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,28 @@
 Rails.application.routes.draw do
+  root "tasks#index"
   devise_for :users
   #counter
-  resources :tictacs
+  resources :tictacs do
+    member do
+      patch :cancel
+      patch :finish
+    end
+  end
+  
   #task
   resources :tasks
   #project
   resources :projects
 
-  root "tasks#index"
+  #Api
+  namespace :api do
+    resources :tictacs, only: [] do
+      member do
+        post :start
+        post :cancel
+        post :finish
+      end
+    end
+  end
 
 end

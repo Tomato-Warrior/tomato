f66e75e755c6215b4b9f79edd1db5009072a206d
yuchan <lumir1031@gmail.com>
2020-05-12 10:05:34 +0800

    add show page

--- app/controllers/tictacs_controller.rb
+++ app/controllers/tictacs_controller.rb
@@ -1,9 +1,10 @@
 class TictacsController < ApplicationController
-  # before_action :current_tictac, only: [:cancel, :finish]
+  before_action :last_tictac, only: [:index, :show, :cancel, :finish]
   
   def index
-    @tictacs = current_user.tictacs
-    @tictac = current_user.tictacs.last
+  end
+
+  def show
   end
 
   def create
@@ -17,13 +18,8 @@ class TictacsController < ApplicationController
       redirect_to tasks_path, notice: 'Tictac failed!'
     end
   end
-
-  def edit
-  end
  
   def cancel
-    @tictac = current_user.tictacs.last
-
     if @tictac.cancel!
       redirect_to tictacs_path, notice: 'Tictac cancelled!'
     else
@@ -32,7 +28,6 @@ class TictacsController < ApplicationController
   end
 
   def finish
-    @tictac = current_user.tictacs.last
     if @tictac.finish!
       redirect_to tictacs_path, notice: 'Tictac finished!'
     else
@@ -40,5 +35,10 @@ class TictacsController < ApplicationController
     end
   end
 
+  private 
+
+  def last_tictac
+    @tictac = current_user.tictacs.last    
+  end
 
 end
--- app/models/tictac.rb
+++ app/models/tictac.rb
@@ -7,20 +7,20 @@ class Tictac < ApplicationRecord
   #aasm
   aasm column: 'status' do
     state :pending, initial: true
-    state :processing, :cancelled, :finished
+    state :active, :cancelled, :finished
   
     event :start do
-      transitions from: :pending, to: :processing
+      transitions from: :pending, to: :active
       after_transaction :status_start
     end
   
     event :cancel do
-      transitions from: :processing, to: :cancelled
+      transitions from: :active, to: :cancelled
       after_transaction :status_cancel
     end
 
     event :finish do
-      transitions from: :processing, to: :finished
+      transitions from: :active, to: :finished
       after_transaction :status_finish
     end
 
@@ -29,7 +29,7 @@ class Tictac < ApplicationRecord
   private
   
   def status_start
-    update(status: 'processing', start_at: Time.now)
+    update(status: 'active', start_at: Time.now)
   end
   def status_cancel
     update(status: 'cancelled', end_at: Time.now)

dc495b11de713dc9d63353717ae3bc4fa6ef1ef9
Linda Wu <pinky510100@gmail.com>
2020-05-14 13:51:56 +0800

    fb & google login

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,13 @@ class User < ApplicationRecord
   after_create :default_project_create
 
   # scope :find_expired_tictac, -> { where(start_at:) }
+  
+  def self.create_from_provider_data(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+      user.email = provider_data.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 
   private
 

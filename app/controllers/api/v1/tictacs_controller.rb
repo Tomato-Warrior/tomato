385bf63b4d33509095c8248a33d454a5c9bee866
yuchan <lumir1031@gmail.com>
2020-05-14 22:12:29 +0800

    移除忘記刪掉的東西

--- app/controllers/api/v1/tictacs_controller.rb
+++ app/controllers/api/v1/tictacs_controller.rb
@@ -1,13 +1,9 @@
 class Api::V1::TictacsController < ApplicationController
-  require 'pry'
   skip_before_action :verify_authenticity_token
   before_action :last_tictac, only: [:cancel, :finish]
 
   def start
-
     @tictac = current_user.tictacs.build(task_id: params[:task_id])
-    
-    find_expired_tictac
 
     if @tictac.start!
       render json: {user_id: @tictac.user_id, status: @tictac.status, start_at: @tictac.start_at, task_id: @tictac.task_id }
@@ -37,13 +33,6 @@ class Api::V1::TictacsController < ApplicationController
 
   def last_tictac
     @tictac = current_user.tictacs.last    
-  end
-
-  def find_expired_tictac
-    if current_user.tictacs.last.status == 'active'
-      current_user.tictacs.last.cancel!
-      render json: { status: @tictac.status, end_at: @tictac.end_at }
-    end
   end
 
 end

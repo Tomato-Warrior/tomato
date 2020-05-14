621abb69ea95b02a3a869730af84ee14338bd314
yuchan <lumir1031@gmail.com>
2020-05-14 21:34:03 +0800

    bundle

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,9 +50,11 @@ class TasksController < ApplicationController
                                  tag_items: []
                                 )
   end
+
   def find_task
     @task = Task.find(params[:id])
   end
+
   def task_list
     @tasks = current_user.tasks.includes(:user)
   end

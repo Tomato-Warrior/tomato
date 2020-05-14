9b521ffafa2683d6cc7fe08e99fe717ae7091c4d
yuchan <lumir1031@gmail.com>
2020-05-14 22:10:20 +0800

    WIP

--- app/controllers/api/v1/tictacs_controller.rb
+++ app/controllers/api/v1/tictacs_controller.rb
@@ -6,23 +6,14 @@ class Api::V1::TictacsController < ApplicationController
   def start
 
     @tictac = current_user.tictacs.build(task_id: params[:task_id])
-    
-    1. 過期的先expired掉
-    2. 撈沒過期的傳回去
+    
+    find_expired_tictac
 
-    if current_user.tictacs.where(status: 'active').last.start_at + 25.minute < Time.now
-      current_user.tictacs.last.finish!
-      render json: { status: @tictac.status, end_at: @tictac.end_at }
-    elsif
-      @tictac.start!
+    if @tictac.start!
       render json: {user_id: @tictac.user_id, status: @tictac.status, start_at: @tictac.start_at, task_id: @tictac.task_id }
+    else
+      render json: { error: "nonoooooo" }, status: 400
     end
-
-    # if @tictac.start!
-    #   render json: {user_id: @tictac.user_id, status: @tictac.status, start_at: @tictac.start_at, task_id: @tictac.task_id }
-    # else
-    #   render json: { error: "nonoooooo" }, status: 400
-    # end
 
   end
 
@@ -47,4 +38,12 @@ class Api::V1::TictacsController < ApplicationController
   def last_tictac
     @tictac = current_user.tictacs.last    
   end
+
+  def find_expired_tictac
+    if current_user.tictacs.last.status == 'active'
+      current_user.tictacs.last.cancel!
+      render json: { status: @tictac.status, end_at: @tictac.end_at }
+    end
+  end
+
 end

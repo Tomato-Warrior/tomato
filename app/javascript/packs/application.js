2a34e45d2ab163e0ee04d314a6f922b108ec4600
Linda Wu <pinky510100@gmail.com>
2020-05-18 21:59:40 +0800

    style/project & task list

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,16 +18,14 @@ import "bootstrap";
 import "./script/main.scss";
 import "./script/_library.scss";
 import "controllers";
-import '@fortawesome/fontawesome-free/js/all';
 
 import 'sweetalert2/src/sweetalert2.scss'
 
-
+import '@fortawesome/fontawesome-free/js/all';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'tempusdominus-bootstrap-4';
 import 'tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.min.css';
 import "select2/dist/css/select2.min.css";
-
 import "select2/dist/js/select2.full.min.js";
 import './home/home.scss'
 

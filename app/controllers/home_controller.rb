af1db380d4c912f953b10e0dab399f5b6e3bd7b9
yuchan <lumir1031@gmail.com>
2020-05-19 01:07:41 +0800

    左側欄layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,8 @@ class HomeController < ApplicationController
   def index
     @projects = Project.all
     @tictacs = Tictac.all
-    tasks = Task.all
     @uncoming_task = Task.without_deleted
     @finish_task = Task.with_deleted
-    
   end
   
 end

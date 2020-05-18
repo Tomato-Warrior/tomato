94be9ccfcf79872c3128f693b846bf1fa99ac215
yuchan <lumir1031@gmail.com>
2020-05-19 02:14:50 +0800

    修正左側欄屬性

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,14 @@ class HomeController < ApplicationController
   layout "home"
   
   def index
-    @projects = Project.all
-    @tictacs = Tictac.all
-    @uncoming_task = Task.without_deleted
+    # @projects = Project.all
+    # @tictacs = Tictac.all
+    # @uncoming_task = Task.without_deleted
+    # @finish_task = Task.with_deleted
+
+    @projects = current_user.projects
+    @tictacs = current_user.tictacs
+    @uncoming_task = current_user.tasks.without_deleted
     @finish_task = Task.with_deleted
   end
   

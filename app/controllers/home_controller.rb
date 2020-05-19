62bdd33c6f947b4e64c4da2157a1687bda0bcdf9
yuchan <lumir1031@gmail.com>
2020-05-19 21:25:12 +0800

    完成show頁面

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,10 @@
 class HomeController < ApplicationController
   
   def index
-    # @projects = Project.all
-    # @tictacs = Tictac.all
-    # @uncoming_task = Task.without_deleted
-    # @finish_task = Task.with_deleted
-
     @projects = current_user.projects
     @tictacs = current_user.tictacs
     @uncoming_task = current_user.tasks.without_deleted
-    @finish_task = Task.with_deleted
+    @finish_task = current_user.tasks.with_deleted
   end
   
 end

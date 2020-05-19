4c379fdfe061b2c1f2517e6399c4196319286d14
Linda Wu <pinky510100@gmail.com>
2020-05-19 19:31:38 +0800

    fix/drag only in project, fix/sound, feat/charts

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -3,23 +3,25 @@ import Sortable from "sortablejs"
 import Rails from "@rails/ujs"
 
 export default class extends Controller {
-  
-  static targets = []
+
+  static targets = [ "project_id" ]
 
   connect() {
     this.sortable = Sortable.create(this.element, {
-        onEnd: this.end.bind(this)
+      onEnd: this.end.bind(this)
     })
   }
   end(e) {
     let id = e.item.dataset.id;
     let data = new FormData();
+    let project_id = this.project_idTarget.dataset.id
+    console.log(project_id)
     data.append("position", e.newIndex + 1);
 
     Rails.ajax({
-        url: this.data.get("url").replace(":id", id),
-        type: 'PATCH',
-        data: data
+      url: `/projects/${project_id}/tasks/:id/drag`.replace(":id", id),
+      type: 'PATCH',
+      data: data
     })
   }
 }

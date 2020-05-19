e8f0cbf70e150a6200314114978405a03e840769
Linda Wu <pinky510100@gmail.com>
2020-05-19 21:57:28 +0800

    refactor/charts

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -1,2 +1,13 @@
 module ChartsHelper
+  def display_today_tictac
+    current_user.tictacs.where(status: "finished").where('end_at BETWEEN ? AND ?', Time.zone.now.beginning_of_day, Time.zone.now.end_of_day).count
+  end
+
+  def display_week_tictac
+    current_user.tictacs.where(status: "finished").where('end_at BETWEEN ? AND ?', Time.zone.now.beginning_of_week, Time.zone.now.end_of_week).count
+  end
+
+  def display_month_tictac
+    current_user.tictacs.where(status: "finished").where('end_at BETWEEN ? AND ?', Time.zone.now.beginning_of_week, Time.zone.now.end_of_week).count
+  end
 end

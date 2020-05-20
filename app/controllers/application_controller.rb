bf6871fc4c3f6a70c89df288f571101dfa29522e
yuchan <lumir1031@gmail.com>
2020-05-20 11:48:07 +0800

    show頁面title跟input修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :nav_find_projects
 
   def nav_find_projects
-    @projects = current_user.projects
+    @projects = current_user.projects.order(updated_at: :desc)
   end
 
 end

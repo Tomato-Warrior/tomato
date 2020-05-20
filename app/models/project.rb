041c02c7469d64cf421e813c5bf645faaab4e89b
kenaser12345 <kenaser12345@gmail.com>
2020-05-20 17:18:48 +0800

    create project by board

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,4 +9,6 @@ class Project < ApplicationRecord
   validates :title, presence: true,
                            length: { minimum: 1, maximum: 20 }
 
+  #一次建立多筆task
+  accepts_nested_attributes_for :tasks
 end

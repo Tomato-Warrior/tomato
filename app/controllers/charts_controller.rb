4436d4bd42afdbc0c6dc455809b2b0a3323f5dc3
Linda Wu <pinky510100@gmail.com>
2020-05-20 12:23:37 +0800

    refactor/dashboard

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,4 +1,15 @@
 class ChartsController < ApplicationController
   def index
+    @today_tictac = current_user.tictacs.where(status: "finished").where('end_at BETWEEN ? AND ?', Time.zone.now.beginning_of_day, Time.zone.now.end_of_day).count
+
+    @week_tictac = current_user.tictacs.where(status: "finished").where('end_at BETWEEN ? AND ?', Time.zone.now.beginning_of_week, Time.zone.now.end_of_week).count
+
+    @month_tictac = current_user.tictacs.where(status: "finished").where('end_at BETWEEN ? AND ?', Time.zone.now.beginning_of_week, Time.zone.now.end_of_week).count
+    
+    @daily_tictac = current_user.tictacs.group(:status).group_by_day(:end_at).count
+    
+    @weekly_tictac = current_user.tictacs.group(:status).group_by_day(:end_at).count
+
+    @monthly_tictac = current_user.tictacs.group(:status).group_by_day(:end_at).count
   end
 end

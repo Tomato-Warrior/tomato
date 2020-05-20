7ad56b5d4946e47980337bc67f7dda8c843e8deb
kenaser12345 <kenaser12345@gmail.com>
2020-05-21 02:56:37 +0800

    匯入trello card

--- app/controllers/trelloapi_controller.rb
+++ app/controllers/trelloapi_controller.rb
@@ -1,14 +1,27 @@
 class TrelloapiController < ApplicationController
   def index
-    Trello.configure do |config|
-      config.developer_public_key = ENV['TRELLO_DEVELOPER_PUBLIC_KEY']
-      config.member_token = ENV['TRELLO_MEMBER_TOKEN']
-    end
+    config_trelo_public_key()
     @me = Trello::Member.find("user50720802")
     @project = Project.new
     @boards = @me.boards
     @board = @boards.find { |board| board.name == "TomaTokei"}
-    @all_list = @board.lists.map{|list| list.name}
+    @all_list = @board.lists.map{|list| list} #["許願池", "To Do", "In Progress", "Done"]
     @all_card = @board.lists.map{|list| list.cards.map{ |card| card.name}}
+    @tasks_attr_data = @board.lists.map{|list| list.cards.map{|card|  "{task_name: '#{card.name}', trello_status: '#{list.name}', user_id: 1}"}}
+    @tasks_attr_data_trans = @tasks_attr_data.map{|list| list.map{|card| eval(card)}}.flatten 
+    load_trello_board()
   end
+
+  private
+  def config_trelo_public_key
+    Trello.configure do |config|
+      config.developer_public_key = ENV['TRELLO_DEVELOPER_PUBLIC_KEY']
+      config.member_token = ENV['TRELLO_MEMBER_TOKEN']
+    end
+  end
+
+  def load_trello_board
+    Project.create!(project_name: @board.name,user_id: 1,
+                    tasks_attributes:@tasks_attr_data_trans)
+  end  
 end

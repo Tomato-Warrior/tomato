165a04180613d60f739ce1ba12a803663c885366
yuchan <lumir1031@gmail.com>
2020-05-21 18:01:57 +0800

    enum修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,9 @@ class HomeController < ApplicationController
   def index
     @projects = current_user.projects
     @tictacs = current_user.tictacs
-    @undone_today_tasks = current_user.tasks.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).where(status: true)
-    @done_today_tasks = current_user.tasks.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).where(status: false)
+    range = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
+    @undone_today_tasks = current_user.tasks.where(created_at: range).doing
+    @done_today_tasks = current_user.tasks.where(created_at: range).done
     @task = Task.new
   end
   

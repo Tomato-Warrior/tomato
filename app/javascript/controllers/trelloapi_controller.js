9d904e4550219f4e3296e503f9ae3b773fd35761
kenaser12345 <kenaser12345@gmail.com>
2020-05-23 17:52:08 +0800

    get client data

--- app/javascript/controllers/trelloapi_controller.js
+++ app/javascript/controllers/trelloapi_controller.js
@@ -3,13 +3,31 @@ import Rails from "@rails/ujs"
 
 export default class extends Controller {
   static targets = []
-
+  trello_token = ""
+  api_key = "f91cef06b7d1a94754eac87835224aeb"
+  
   authenticationSuccess = function() {
     console.log('Successful authentication')
+    
   }
   
   authenticationFailure = function() {
     console.log('Failed authentication')
+  }
+  trelloAuthorize = function() {
+    window.Trello.authorize({
+      type: 'popup',
+      name: 'start',
+      scope: {
+        read: 'true',
+        write: 'true' },
+      expiration: 'never',
+      success:  () => {
+                      this.authenticationSuccess()
+                      this.trello_token = localStorage.trello_token
+                      },
+      error: this.authenticationFailure
+    })
   }
 
   connect(){
@@ -17,17 +35,31 @@ export default class extends Controller {
 
   get_token(e){
     e.preventDefault()
-    window.Trello.authorize({
-      type: 'popup',
-      name: 'Getting Started Application',
-      scope: {
-        read: 'true',
-        write: 'true' },
-      expiration: 'never',
-      success: this.authenticationSuccess,
-      error: this.authenticationFailure
-    })
+    this.trelloAuthorize() 
   }
 
+  add_task(e){
+    e.preventDefault()
+    fetch(`https://api.trello.com/1/members/me/boards?key=${this.api_key}&token=${this.trello_token}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => {
+      console.log(
+        `Response: ${response.status} ${response.statusText}`
+      );
+      return response.text();
+    })
+    .then(text => console.log(text))
+    .catch(err => console.error(err));
+
+  
+    /*const boards = window.Trello.get('/members/me/boards');
+    console.log(boards)
+    console.log(boards)*/
+    
+  }
 
 }

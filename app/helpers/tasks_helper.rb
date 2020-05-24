a3ba499e7f6999e79c03727abeb616528a1d6134
Linda Wu <pinky510100@gmail.com>
2020-05-24 22:59:40 +0800

    fix/display_tag, feat/display_expect_tictac

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,5 +1,19 @@
 module TasksHelper
+
   def display_tag( tag_names )
-    tag_names.map{|tag|"<span class='badge tag-color'>#{tag}</span>"}.join(' ').html_safe
+    tag_names.map{|tag|"<span class='badge tag-color mx-1'>#{tag}</span>"}.join(' ').html_safe
   end
+
+  def display_expect_tictac( tictac_num )
+    if tictac_num != nil
+      i = 0
+      expect_tictac = ''
+      while i < tictac_num
+        i += 1
+        expect_tictac << "<img src='/expect_tictac.png' style='max-width:20px'>"
+      end
+      expect_tictac.html_safe
+    end
+  end
+
 end

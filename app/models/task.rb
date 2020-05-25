31599b6cff76a04ce177c8e0ae5aee98d4b07a13
yuchan <lumir1031@gmail.com>
2020-05-25 14:29:53 +0800

    修改完view的方法

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,33 +10,33 @@ class Task < ApplicationRecord
   belongs_to :project
   belongs_to :user
   #validates
-  validates :task_name, presence: true
+  validates :title, presence: true
   enum status: {doing: 0 , done: 1 }
 
   #tag
   def self.tagged_with(name)
-    Tag.find_by!(tag_name: name).tasks
+    Tag.find_by!(name: name).tasks
   end
 
   def tag_list=(names)
     self.tags = names.split(',').map do |item|
-      Tag.where(tag_name: item.strip).first_or_create!
+      Tag.where(name: item.strip).first_or_create!
     end
   end
 
   def tag_list
-    tags.map(&:tag_name).join(',')
+    tags.map(&:name).join(',')
   end
 
   def tag_items=(names)
     cur_tags = names.map do |item| 
-      Tag.where(tag_name: item.strip).first_or_create! unless item.blank?
+      Tag.where(name: item.strip).first_or_create! unless item.blank?
     end.compact
     self.tags = cur_tags
   end
 
   def tag_items
-    tags.map(&:tag_name)
+    tags.map(&:name)
   end
 
   private

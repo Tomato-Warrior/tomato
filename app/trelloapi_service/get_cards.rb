90a7da672030dc249f8eed921ef73ae17ed9ea78
kenaser12345 <kenaser12345@gmail.com>
2020-05-26 02:37:54 +0800

    可選擇Trello list新建任務

---
+++ app/trelloapi_service/get_cards.rb
@@ -0,0 +1,13 @@
+class GetCards
+  require 'rest-client'
+  def initialize()
+  end
+
+  def get_cards(board_id, api_key, token)
+    begin
+      RestClient.get "api.trello.com/1/boards/#{board_id}/cards?key=#{api_key}&token=#{token}"
+    rescue RestClient::ExceptionWithResponse => e
+      e.response
+    end
+  end
+end

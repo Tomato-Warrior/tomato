36a08e1ca1f37f3978f9596733c75843b492575e
Linda Wu <pinky510100@gmail.com>
2020-05-27 12:04:44 +0800

    調整left_nav對齊,刪除重複CSS

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @projects = current_user.projects
     range = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
     @tictacs = current_user.tictacs.where(end_at: range).finished
     @undo_today_tasks = current_user.tasks.where(created_at: range).doing

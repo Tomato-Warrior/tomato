bfc1e43342b35211c50e4142b7737c4ed401fa36
yuchan <lumir1031@gmail.com>
2020-05-27 09:26:22 +0800

    左側欄增加未完成任務

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :nav_find_projects, :today_expect_time_of_tasks
+  before_action :nav_find_projects, :today_expect_time_of_tasks, :undo_tasks
   
   def nav_find_projects
     @projects = current_user.projects.includes(:user).order(updated_at: :desc) if user_signed_in?
@@ -13,10 +13,24 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def expect_time
+  def undo_tasks
+    if user_signed_in?
       range = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
-      tictac_hours = current_user.tasks.where(created_at: range).sum(:expect_tictacs) * 1500.0 / 3600 
-      tictac_hours.round(tictac_hours % 10 == 0 ? 0 : 1)
+      @undo_tasks = current_user.tasks.where(created_at: range).doing.count
+    end
+  end
+
+  
+  private
+  def expect_time
+    range = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
+    tictac_hours = current_user.tasks.where(created_at: range).sum(:expect_tictacs) * 1500.0 / 3600 
+    tictac_hours.round(tictac_hours % 10 == 0 ? 0 : 1)
+  end
+
+  def undoing_tasks
+    range = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
+    current_user.tasks.where(created_at: range).doing.count
   end
 
 end

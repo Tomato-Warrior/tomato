f37a7510ea623315b753a67ebf2a4968fbc2a09b
kenaser12345 <kenaser12345@gmail.com>
2020-05-28 15:55:56 +0800

    import assigned card

--- app/trelloapi_service/get_boards.rb
+++ app/trelloapi_service/get_boards.rb
@@ -11,5 +11,12 @@ class GetBoards
       e.response
     end
   end
-  
+
+  def get_board_name(board_id, api_key, token)
+    begin
+      RestClient.get "api.trello.com/1/boards/#{board_id}?key=#{api_key}&token=#{token}"
+    rescue RestClient::ExceptionWithResponse => e
+      e.response
+    end
+  end
 end

58cdff2678fe11afd83f9dba03a199a42ad1515a
kenaser12345 <kenaser12345@gmail.com>
2020-05-29 16:25:52 +0800

    fix ASCII problem

--- app/trelloapi_service/get_cards.rb
+++ app/trelloapi_service/get_cards.rb
@@ -1,9 +1,18 @@
 class GetCards
   require 'rest-client'
-
+  require 'uri'
   def get_cards(board_id, api_key, token)
     begin
       RestClient.get "api.trello.com/1/boards/#{board_id}/cards?key=#{api_key}&token=#{token}"
+    rescue RestClient::ExceptionWithResponse => e
+      e.response
+    end
+  end
+
+  def get_list_card(board_name, list_name, api_key, token)
+    url = "api.trello.com/1/search?query=board:#{board_name}%20list:#{list_name}%20sort:edited&card_fields=name,idList&cards_limit=1000&key=#{api_key}&token=#{token}".force_encoding('ASCII-8BIT')
+    begin
+      RestClient.get URI::encode(url)
     rescue RestClient::ExceptionWithResponse => e
       e.response
     end

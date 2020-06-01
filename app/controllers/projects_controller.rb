36bd544e03465ba9cc67ee5df5a69c1b838ae8dd
yuchan <lumir1031@gmail.com>
2020-06-01 13:49:23 +0800

    bug

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,7 +14,7 @@ class ProjectsController < ApplicationController
     if @project.save
       redirect_to project_path(@project)
     else
-      render :newx
+      render :new
     end
   end
   

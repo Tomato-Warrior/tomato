ab4afcf0bd5a6f65cd4aac8fe7c375a9a86f3309
kenaser12345 <kenaser12345@gmail.com>
2020-06-01 19:01:07 +0800

    done

--- app/controllers/trelloapi_controller.rb
+++ app/controllers/trelloapi_controller.rb
@@ -21,6 +21,7 @@ class TrelloapiController < ApplicationController
     list_id = params[:list_id]
     task_id = params[:task_id]
     response = UpdateCard.new.move_to_list(card_id, list_id, ENV['TRELLO_DEVELOPER_PUBLIC_KEY'], ENV['TRELLO_USER_TOKEN'])
+    
     render json: {res: response}
     Task.find(task_id).trello_info.update(list_id: list_id)
     #製作巢狀參數                  

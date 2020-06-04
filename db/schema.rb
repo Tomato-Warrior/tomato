3c4db81c41c09164d5b8e8fab5371694408a93ff
Linda Wu <pinky510100@gmail.com>
2020-06-04 19:30:06 +0800

    fix/conflict, 縮排

--- db/schema.rb
+++ db/schema.rb
@@ -93,10 +93,7 @@ ActiveRecord::Schema.define(version: 2020_06_01_054811) do
     t.string "provider"
     t.string "uid"
     t.string "auth_token"
-<<<<<<< HEAD
     t.string "trello_token"
-=======
->>>>>>> add auth_toke, auth_api, api_controller
     t.index ["auth_token"], name: "index_users_on_auth_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

5ac253196f05dff27e75393eb304ea1bb05bae2c
kenaser12345 <kenaser12345@gmail.com>
2020-06-04 19:18:21 +0800

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_06_01_054811) do
-
+ActiveRecord::Schema.define(version: 2020_06_04_084312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,7 +80,9 @@ ActiveRecord::Schema.define(version: 2020_06_01_054811) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "board_id"
+    t.bigint "user_id", null: false
     t.index ["task_id"], name: "index_trello_infos_on_task_id"
+    t.index ["user_id"], name: "index_trello_infos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -107,5 +107,5 @@ ActiveRecord::Schema.define(version: 2020_06_01_054811) do
   add_foreign_key "tasks", "users"
   add_foreign_key "tictacs", "users"
   add_foreign_key "trello_infos", "tasks"
+  add_foreign_key "trello_infos", "users"
 end
-

7c87c1a2c79462c766d07e8dd56a3dd14fb770a4
kenaser12345 <kenaser12345@gmail.com>
2020-06-05 11:58:19 +0800

    find current user

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,14 +1,14 @@
 class WebhooksController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!
 
-  $response
   def complete
+    byebug
     render status: 200
   end
 
   def receive
-    $response = JSON.parse(request.body.read)
-    res = $response
+    byebug
+    res = JSON.parse(request.body.read)
     action = Webhook.new.action(res)
     board = Webhook.new.board(res)
     if action == "updateCard"
@@ -18,10 +18,8 @@ class WebhooksController < ApplicationController
       @target_task = current_user.trello_infos.where(card_id: card_id)[0].task
       @target_task.trello_info.update(list_id: @after_list[0])
     elsif action == "createCard"
-      #新建卡片
       new_card = Webhook.new.card(res)
       card_position = Webhook.new.current_list(res)
-      byebug
     end
   end
 end

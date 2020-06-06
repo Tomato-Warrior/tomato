bdf354f2ab0582984f72fb9b5175f3f157bac021
yuchan <lumir1031@gmail.com>
2020-06-06 20:17:20 +0800

    info資訊rwd

--- app/javascript/Vue/components/todo/info.vue
+++ app/javascript/Vue/components/todo/info.vue
@@ -1,33 +1,41 @@
 <template>
   <div class="show-data mx-3 my-3">
     <h3>任務資訊</h3>
-    <div class="data-container d-flex">
-      <div class="col-3">
+    <div class="data-container d-flex flex-wrap">
+      <div class="col-6 col-md-3">
         <div class="data">
           <p>預計時間</p>
-          <span class="tomato-num">{{ expect_time }}</span>
-          <small>小時</small>
+          <div class="">
+            <span class="tomato-num">{{ expect_time }}</span>
+            <small>小時</small>
+          </div>
         </div>
       </div>
-      <div class="col-3">
+      <div class="col-6 col-md-3">
         <div class="data">
           <p>待完成任務</p>
-          <span class="doing-task-num">{{ doingTasks.length }}</span>
-          <small>個</small>
+          <div>
+            <span class="doing-task-num">{{ doingTasks.length }}</span>
+            <small>個</small>
+          </div>
         </div>
       </div>
-      <div class="col-3">
+      <div class="col-6 col-md-3">
         <div class="data">
           <p>收集番茄</p>
-          <span class="tomato-num">{{ finish_tictac }}</span>
-          <small>個</small>
+          <div class="">
+            <span class="tomato-num">{{ finish_tictac }}</span>
+            <small>個</small>
+          </div>
         </div>
       </div>
-      <div class="col-3">
+      <div class="col-6 col-md-3">
         <div class="data">
           <p>已完成任務</p>
-          <span class="done-data-num">{{ doneTasks.length }}</span>
-          <small>個</small>
+          <div>
+            <span class="done-data-num">{{ doneTasks.length }}</span>
+            <small>個</small>
+          </div>
         </div>
       </div>
       </div>

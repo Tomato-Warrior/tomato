4e14505049dad9b463e8252570adcb9e8b4cc746
kenaser12345 <kenaser12345@gmail.com>
2020-06-08 19:22:30 +0800

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_075507) do
+ActiveRecord::Schema.define(version: 2020_06_08_064903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2020_06_07_075507) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "board_id"
     t.bigint "user_id", null: false
+    t.string "list_name"
     t.index ["task_id"], name: "index_trello_infos_on_task_id"
     t.index ["user_id"], name: "index_trello_infos_on_user_id"
   end

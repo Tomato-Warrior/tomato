ebdb8ed3b86751057fd67fb74547a8aa694091c9
kenaser12345 <kenaser12345@gmail.com>
2020-06-09 23:34:55 +0800

    ngrok

--- db/migrate/20200607075507_add_column_to_project_for_trello.rb
+++ db/migrate/20200607075507_add_column_to_project_for_trello.rb
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 class AddColumnToProjectForTrello < ActiveRecord::Migration[6.0]
   def change
     add_column :projects, :trello_import_method, :string
   end
 end
-=======
-class AddColumnToProjectForTrello < ActiveRecord::Migration[6.0]
-  def change
-    add_column :projects, :trello_import_method, :string
-  end
-end
->>>>>>> 選擇匯入指定任務不後不會更新出不是自己的卡片

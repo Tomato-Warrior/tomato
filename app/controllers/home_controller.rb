289fc2a2f00ee75926aa869a6fdc0f65815e76e8
Linda Wu <pinky510100@gmail.com>
2020-06-10 23:11:45 +0800

    fix/home_task_count

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,20 @@ class HomeController < ApplicationController
   def index
     range = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
     @tictacs = current_user.tictacs.where(end_at: range).finished
-    @undo_today_tasks = current_user.tasks.where(created_at: range).doing
-    @done_today_tasks = current_user.tasks.where(created_at: range).done
+    tasks = current_user.tasks
+    @undo_today_tasks = tasks.where(created_at: range).doing
+    @done_today_tasks = tasks.where(created_at: range).done
     @task = Task.new
-    @user = current_user
+
+    today_new_tasks = tasks.where(created_at: range)
+    @today_trello_new_task = 0
+    today_new_tasks.each do |task|
+      if task.trello_info.present?
+        @today_trello_new_task += 1
+      end
+    end
+    @today_new_tasks = today_new_tasks.count - @today_trello_new_task
+
   end
 
   def todo

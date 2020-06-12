300ba1db0649c4611613a962fc39d7ee0fc94073
kenaser12345 <kenaser12345@gmail.com>
2020-06-12 10:51:52 +0800

    remove foreign key

--- db/migrate/20200604084312_add_column_to_trello_info.rb
+++ db/migrate/20200604084312_add_column_to_trello_info.rb
@@ -1,5 +1,5 @@
 class AddColumnToTrelloInfo < ActiveRecord::Migration[6.0]
   def change
-    add_reference :trello_infos, :user, null: false, foreign_key: true
+    add_reference :trello_infos, :user
   end
 end

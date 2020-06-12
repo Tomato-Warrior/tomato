bf9829d295b6b9d9479b9dbba89d17cf80c3840a
Lindawu <61684249+lindawu100@users.noreply.github.com>
2020-06-12 11:25:16 +0800

    Revert "選擇匯入指定任務不後不會更新出不是自己的卡片"
    This reverts commit 8eb652e80ec30b32d1fa080f8ca9bd16e2351726.

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -3,24 +3,26 @@ class WebhooksController < ApplicationController
 
   $data
   def complete
+    @data = $data
     render status: 200
   end
 
   def receive
     res = JSON.parse(request.body.read)
+    $data = res
     member_id = Webhook.new.member_id(res)
     action = Webhook.new.action(res)
     board = Webhook.new.board(res)
     user = User.find_by(trello_member_id: member_id)
-    target_project = user.projects.where(title: board.values_at("name"))[0]   
     if action == "updateCard"
       card_id = Webhook.new.card(res).values_at("id").join
       @after_list = Webhook.new.after_list(res)
       target_task = user.trello_infos.where(card_id: card_id)[0].task
       target_task.trello_info.update(list_id: @after_list[0])
-    elsif action == "createCard" && target_project.trello_import_method == "import_all_card"
+    elsif action == "createCard"
       new_card = Webhook.new.card(res)
       card_position = Webhook.new.current_list(res)
+      target_project = user.projects.where(title: board.values_at("name"))[0]
       new_task = target_project.tasks.create(title: new_card.values_at("name").join, user_id: user.id)
       new_task.create_trello_info({card_id:new_card.values_at("id")[0], 
                                   list_id:card_position.values_at("id")[0], 
@@ -28,7 +30,8 @@ class WebhooksController < ApplicationController
                                   user_id:user.id})
     elsif action == "addMemberToCard"
       member_id = Webhook.new.assign_member_id(res)
-      card_name = Webhook.new.card(res).values_at("name").join  
+      card_name = Webhook.new.card(res).values_at("name").join
+      target_project = user.projects.where(title: board.values_at("name"))[0]       
       if user.trello_member_id == member_id && target_project.tasks.map{|task| task.title}.include?(card_name) == false
         assign_card = Webhook.new.card(res)
         assign_card_list = JSON.parse(GetCards.new.get_card_by_id(assign_card.values_at("id")[0], ENV['TRELLO_DEVELOPER_PUBLIC_KEY'], user.trello_token))
--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,7 @@
 
 
 
-
 ActiveRecord::Schema.define(version: 2020_06_09_022210) do
-
 
 
   # These are extensions that must be enabled in order to support this database

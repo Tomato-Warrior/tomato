e02a35ad711a88026c3bb84a4ac26a004d812b16
Linda Wu <pinky510100@gmail.com>
2020-06-12 10:01:39 +0800

    fix/trello_chart

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :find_project, only: [:edit, :update, :destroy, :show]
+  before_action :find_project, only: [:edit, :update, :destroy, :show, :chart]
   
   def index
     current_user.projects.includes(:user) if user_signed_in?
@@ -36,7 +36,6 @@ class ProjectsController < ApplicationController
     task_ids = @project.tasks.ids
     @tictac_count = Tictac.where(task_id: task_ids).finished.count
     @project_expect_time = project_expect_time
-    @isTrello = @project.trello_board_id?
   end
 
   def destroy
@@ -47,6 +46,63 @@ class ProjectsController < ApplicationController
       Webhook.new.delete(@project.webhook_id, ENV['TRELLO_DEVELOPER_PUBLIC_KEY'], current_user.trello_token) if @project.webhook_id
       redirect_to root_path
     end
+  end
+
+  def chart
+    # pie chart
+    @trello_project_finished = 0
+    @trello_project_cancelled = 0
+    trello_board_projects = []
+
+    if @project.trello_board_id != nil
+      trello_board_projects = Project.where(trello_board_id: @project.trello_board_id)
+      trello_board_projects.each do |project|
+        project.tasks.each do |task|
+          @trello_project_finished += task.tictacs.finished.count       
+          @trello_project_cancelled += task.tictacs.cancelled.count
+        end
+      end
+    end
+
+    # data table
+    @task_finished_finished = 0
+    @task_cancelled_cancelled = 0
+    trello_infos = []
+    trello_tasks_id = []
+
+    if @project.trello_board_id != nil
+      trello_infos = TrelloInfo.where(board_id: @project.trello_board_id)
+
+      trello_infos.each do |trello_info|
+        trello_tasks_id << trello_info.task_id
+        trello_tasks_id.uniq!
+      end
+
+      @tasks_arr = []
+      trello_tasks_id.each do |trello_task_id|
+        @task_arr = []
+        trello_infos.each do |trello_info|
+          @title_arr = []
+          @finish_arr = []
+          @cancel_arr = []
+
+          if trello_info.task_id == trello_task_id
+            @title_arr << trello_info.task.title
+            @finish_arr << trello_info.task.tictacs.finished.count
+            @cancel_arr << trello_info.task.tictacs.cancelled.count
+          end
+          @title = @title_arr.uniq.join
+          @finished_total = @finish_arr.sum
+          @cancel_total = @cancel_arr.sum
+          
+          if trello_info.task_id == trello_task_id
+            @task_arr.push(@title, @finished_total, @cancel_total)
+          end
+        end
+        @tasks_arr << @task_arr
+      end
+    end
+
   end
 
   private
